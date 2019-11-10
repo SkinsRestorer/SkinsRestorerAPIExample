c933687214ace98de343bad3921ce84b792a3781
McLive <mail@mclive.eu>
2019-11-10 13:51:45 +0100

    Demonstrate how to set skin using the API

--- src/main/java/net/skinsrestorer/SkinsRestorerAPIExample.java
+++ src/main/java/net/skinsrestorer/SkinsRestorerAPIExample.java
@@ -1,8 +1,12 @@
 package net.skinsrestorer;
 
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import skinsrestorer.bukkit.SkinsRestorer;
+import skinsrestorer.bukkit.SkinsRestorerBukkitAPI;
+import skinsrestorer.shared.exception.SkinRequestException;
 
 /**
  * Created by McLive on 30.08.2019.
@@ -10,6 +14,7 @@ import skinsrestorer.bukkit.SkinsRestorer;
 public class SkinsRestorerAPIExample extends JavaPlugin {
     private CommandSender console;
     private SkinsRestorer skinsRestorer;
+    private SkinsRestorerBukkitAPI skinsRestorerBukkitAPI;
 
     public void onEnable() {
         console = getServer().getConsoleSender();
@@ -18,5 +23,37 @@ public class SkinsRestorerAPIExample extends JavaPlugin {
         skinsRestorer = JavaPlugin.getPlugin(SkinsRestorer.class);
 
         console.sendMessage(skinsRestorer.toString());
+
+        skinsRestorerBukkitAPI = skinsRestorer.getSkinsRestorerBukkitAPI();
+
+        console.sendMessage(skinsRestorerBukkitAPI.toString());
+
+        this.getCommand("api").setExecutor(this);
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("You can only run this command as a player.");
+            return true;
+        }
+
+        if (args.length < 1) {
+            sender.sendMessage("/api <skin name>");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        String skin = args[0];
+
+        player.sendMessage("Setting your skin to " + skin);
+
+        try {
+            skinsRestorerBukkitAPI.setSkin(player.getName(), skin);
+            skinsRestorerBukkitAPI.applySkin(player);
+        } catch (SkinRequestException e) {
+            e.printStackTrace();
+        }
+
+        return true;
     }
 }
