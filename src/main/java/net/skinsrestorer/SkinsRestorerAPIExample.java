2d9a17512b8fc9fea0f270fe63d02bb18553b5c2
xknat <kevin.natte@gmail.com>
2020-07-24 00:01:59 +0200

    Update SkinsRestorerAPIExample.java

--- src/main/java/net/skinsrestorer/SkinsRestorerAPIExample.java
+++ src/main/java/net/skinsrestorer/SkinsRestorerAPIExample.java
@@ -4,6 +4,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+//Skinsrestorer imports!
 import skinsrestorer.bukkit.SkinsRestorer;
 import skinsrestorer.bukkit.SkinsRestorerBukkitAPI;
 import skinsrestorer.shared.exception.SkinRequestException;
@@ -13,19 +15,21 @@ import skinsrestorer.shared.exception.SkinRequestException;
  */
 public class SkinsRestorerAPIExample extends JavaPlugin {
     private CommandSender console;
+    // Setting definition
     private SkinsRestorer skinsRestorer;
     private SkinsRestorerBukkitAPI skinsRestorerBukkitAPI;
 
+
     public void onEnable() {
         console = getServer().getConsoleSender();
-
         console.sendMessage("Loading SkinsRestorer API...");
+        
+        //Connecting to the main SkinsRestorer API
         skinsRestorer = JavaPlugin.getPlugin(SkinsRestorer.class);
-
         console.sendMessage(skinsRestorer.toString());
 
+        // Connecting to Bukkit API for applying the skin
         skinsRestorerBukkitAPI = skinsRestorer.getSkinsRestorerBukkitAPI();
-
         console.sendMessage(skinsRestorerBukkitAPI.toString());
 
         this.getCommand("api").setExecutor(this);
@@ -47,8 +51,11 @@ public class SkinsRestorerAPIExample extends JavaPlugin {
 
         player.sendMessage("Setting your skin to " + skin);
 
+        
         try {
+            // setskin for player skin 
             skinsRestorerBukkitAPI.setSkin(player.getName(), skin);
+            // Force skinrefresh for player
             skinsRestorerBukkitAPI.applySkin(player);
         } catch (SkinRequestException e) {
             e.printStackTrace();
