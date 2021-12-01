2b4143cea2f23720d93015812e9f1dacd1930d61
xknat <kevin.natte@gmail.com>
2021-12-01 14:20:23 +0100

    wip

--- src/main/java/net/skinsrestorer/SkinsRestorerAPIExample.java
+++ src/main/java/net/skinsrestorer/SkinsRestorerAPIExample.java
@@ -3,7 +3,6 @@ package net.skinsrestorer;
 import net.skinsrestorer.api.PlayerWrapper;
 import net.skinsrestorer.api.SkinsRestorerAPI;
 import net.skinsrestorer.api.exception.SkinRequestException;
-import net.skinsrestorer.api.property.IProperty;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -51,12 +50,14 @@ public class SkinsRestorerAPIExample extends JavaPlugin {
         player.sendMessage(ChatColor.AQUA + "Setting your skin to " + skin);
 
         try {
+            /* WIP
+
         // /api custom
         if (skin == "custom") {
             String value = "ewogICJ0aW1lc3RhbXAiIDogMTYzODM1OTAzNzI2MSwKICAicHJvZmlsZUlkIiA6ICI3ZGEwMzQ4NDU4MTY0OGNjYjEwNTRjZjdmZDZiMjQxYyIsCiAgInByb2ZpbGVOYW1lIiA6ICJSYWNoeGVsIiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzFkODNiNzY2MjNmMjVhMzljYzE5NjExMWIzZjZlOTAyZmE4NGVlODUxYWVhYWNlMTU1MzQzYjBhNjc0MTI0YjgiLAogICAgICAibWV0YWRhdGEiIDogewogICAgICAgICJtb2RlbCIgOiAic2xpbSIKICAgICAgfQogICAgfSwKICAgICJDQVBFIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS8yMzQwYzBlMDNkZDI0YTExYjE1YThiMzNjMmE3ZTllMzJhYmIyMDUxYjI0ODFkMGJhN2RlZmQ2MzVjYTdhOTMzIgogICAgfQogIH0KfQ=";
             String signature = "GH2ECR/QU9vt7x6GLEQrgYsy/kVkJtVsApA04g/d3pcrlJNx9Q7oat4GNYM++cTXPQTQ7Cc2B/hHZnn4ZZfI5p2YYJ+3R1P6ci6zArVYfgRnsrwSR2JSfP3e6iXvctpkvGzIYuqJXbRR914Ie0xCxzjpwUwjINGZGjnQKzZdUD11OaheGRbp6wgf6vCIoStesf/MrXoqKbDYzsrO5jvbb09HszA3DoFXoh4KqvlwDJMur8awOi1+lqIvCGDvszsjkNgfHwQCEfUGduO66LtLK+WRluXzSBXd9hNsXwytrapcNnVZxj3DQOyZjV8LU5hP5RJg9ybO+/+UOAvat7QyhMf/wkMGipENvjVa/bW7KNRBnVnINxkTfskOz+MS8bhe9PjJmmdcmTSAqpcB/cKNlT3lgIqu3GiLOBmGxeT/h4tpncNPsH1q/OEhJE2kFo53GGR7y30Y/C8KGjGx2EdRXMI7IYXBTvxfC5daEPFoO8vlNxkP9ChmBRwfeCeZyUs9HqE6+T7JYgMt/ZAicMrEtz65ds0z4zw5xxgU6vDSGGBZ9JJqfpK384PKxKAAb8fPYTKT7MR7mBxE/e3/tcnW4yNdu7r8UQnJ6JtQQPyBVLUIHCv+liep5yfK0IrRlD0VqJ9aDfs0sltN9lhcW+Lyn+Zwaoa6YS3stERO3mobOsY=";
             skinsRestorerAPI.setSkinData("custom", new GProperty("textures", value, signature), null);
-        }
+        }*/
 
             // #setSkin() for player skin
             skinsRestorerAPI.setSkin(player.getName(), skin);
